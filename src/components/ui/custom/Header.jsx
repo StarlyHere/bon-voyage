3684fc79af4bc3e28a0f55f6a185f8a02ef86fe1
Anushka Priyadarshi <anushka1665@gmail.com>
2025-03-30 14:24:43 +0530

    Feat: UI changes

--- src/components/ui/custom/Header.jsx
+++ src/components/ui/custom/Header.jsx
@@ -3,8 +3,8 @@ import { Button } from '../button'
 
 function Header() {
   return (
-    <div className='p-2 shadow-sm flex justify-between items-center px-5'>
-        <img src='/logo.svg'/>
+    <div className='p-2 bg-transparent fixed top-0 w-full flex justify-between items-center px-5 z-10'>
+        <img src='/logo.svg' alt="Logo" className="h-12" />
         <div>
             <Button>Sign In</Button>
         </div>
--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -4,19 +4,23 @@ import { Button } from '../button';
 
 function Hero() {
   return (
-    <div className='flex flex-col items-center mx-56 gap-9'>
-      <h1 className='font-extrabold text-[50px] text-center mt-16'>
-        <span className='text-[#A35C7A]'>Discover Your Next Adventure</span>
-      </h1>
-      <p className='text-xl text-[#C890A7] text-center'>
-        Your personal travel planner
-      </p>
+    <div 
+      className="min-h-screen w-full bg-cover bg-center flex flex-col items-center justify-center"
+      style={{ backgroundImage: "url('/ocean.jpg')" }}
+    >
+      <div className='flex flex-col items-center mx-8 md:mx-56 gap-9 bg-[#D9D9D9] bg-opacity-20 p-10 rounded-lg'>
+        <h1 className='font-extrabold text-[50px] text-center mt-4'>
+          <span className='text-[#D9D9D9]'>Discover Your Next Adventure</span>
+        </h1>
+        <p className='text-xl text-[#D9D9D9] text-center'>
+          Your personal travel planner
+        </p>
 
-      <Link to ={'/create-trip'}>
-        <Button>Get Started, It's</Button>
-      </Link>
+        <Link to={'/create-trip'}>
+          <Button className="bg-[#007A8C]">Get Started, It's</Button>
+        </Link>
+      </div>
     </div>
-  
   );
 }
 
